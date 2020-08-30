bb41e4fc20ee0e359e854013019c90829b0f271f
Lucas Boulding <lucas@bonobo.ac.uk>
2020-08-30 21:46:39 +0100

    Refactored round 1 calculations into a function

--- monrad_round1.py
+++ monrad_round1.py
@@ -6,6 +6,23 @@ from operator import attrgetter
 from elo_ratings import new_ratings
 from players import Player
 
+def round_one(players):
+    """ This function takes a list of Player objects, and prints the first round pairing. In the first round of Monrad, the pairing is always the same.
+    """
+    ### This uses iterator approach to pairing items in a list
+    paired = zip_longest(players[::2], players[1::2])
+    zipped_paired = list(paired)
+
+    for item in zipped_paired:
+        if item[1] == None:
+            print("Seed", item[0].seed, item[0].name, "gets a bye.")
+            item[0].bye = True
+        else:
+            print("Seed", item[0].seed, item[0].name, "vs. Seed", item[1].seed, item[1].name)
+            item[0].played.append(item[1].seed)
+            item[1].played.append(item[0].seed)
+
+
 # Arbitrary dict of players
 players = {"Timmy Whippet": 1181,
             "Margo Bunt": 1089,
@@ -21,7 +38,7 @@ players = {"Timmy Whippet": 1181,
             "Elsbeth Bethels": 756,
             "Functional Glibro": 713,
             "Patricia Partridge": 653,
-            #"Stefan Funke": 581,
+            "Stefan Funke": 581,
             "Jim Dynamo": 410,}
 
 # Transform dict into list of player objects
@@ -40,38 +57,9 @@ for player in sorted_players:
     player.seed = seed_rank
     seed_rank += 1
 
+# Print out first round matches and note who has played / got a bye
+round_one(sorted_players)
 
-sorted_copy = list(sorted_players)
-
-
-match_number = 1
-# while len(sorted_copy) != 0:
-#     x,y = sorted_copy[:2]
-#     print("Match number {} is between seed {} {} and seed {} {}".format(match_number, x.seed, x.name, y.seed, y.name))
-#     x.played = x.played.append(y.seed)
-#     y.played = y.played.append(x.seed)
-#     sorted_copy.pop(0)
-#     sorted_copy.pop(0)
-#     match_number += 1
-
-# for x in sorted_copy:
-#     for y in sorted_copy:
-#         print("Match number {} is between seed {} {} and seed {} {}".format(match_number, x.seed, x.name, y.seed, y.name))
-### This solution works, but depopulates the list and doesn't return the
-### seed information to the real object, only to the copy.
-
-### This uses iterator approach
-paired1 = zip_longest(sorted_copy[::2], sorted_copy[1::2])
-zipped_paired1 = list(paired1)
-
-for item in zipped_paired1:
-    if item[1] == None:
-        print("Seed", item[0].seed, item[0].name, "gets a bye.")
-        item[0].bye = True
-    else:
-        print("Seed", item[0].seed, item[0].name, "vs. Seed", item[1].seed, item[1].name)
-        item[0].played.append(item[1].seed)
-        item[1].played.append(item[0].seed)
 
 
 for player in sorted_players:
